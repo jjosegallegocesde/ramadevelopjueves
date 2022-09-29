d13db99467014c9d1fa92454cfd02e56017891be
alejaa17 <308@academicos.cesde.edu.co>
2022-09-29 08:04:21 -0500

    Se crea funcion para multiplicar

---
+++ funcion2.py
@@ -0,0 +1,11 @@
+def multiplicar(numero1):
+
+    return numero1 * 100
+
+
+print(f'El resultado es {multiplicar(3)}')    
+
+
+multiplicarlambda = lambda numero1: numero1*100
+
+print(f'El resultado es {multiplicarlambda(3)}')
