e8950b3b2f728cad0e2547ea809774c1b43d53c8
Darka304 <esteban.1123@hotmail.com>
2022-09-29 07:18:18 -0500

    NTJ9 se crean funciones lambda

---
+++ funcion1.py
@@ -0,0 +1,7 @@
+def sumar(n1, n2):
+    return n1 + n2
+    
+print(sumar(5,2))
+
+sumarL=lambda n1L,n2L:n1L+n2L
+print(sumarL(5,3))
--- johanDiaz.py
+++ johanDiaz.py
@@ -1,1 +1,12 @@
 #("*FUNCIONES LAMBA*")
+
+def saludar(nombre):
+    return "Hola " + nombre
+
+print(saludar("Johan"))
+
+#LAMBDA
+
+saludarL=lambda nombreL:"Hola " + nombreL
+
+print(saludarL("Esteban"))
