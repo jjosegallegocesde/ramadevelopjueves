b83b1c217ec1de9e58df90fe00d3c841a451edb3
alejaa17 <308@academicos.cesde.edu.co>
2022-09-29 07:22:51 -0500

    NTJ9 Se crea funcion para sumar

---
+++ funcion1.py
@@ -0,0 +1,16 @@
+#funcion que sume dos numeros 
+
+
+
+
+def sumar(numero1,numero2):
+    return numero1+numero2    
+
+
+print(f'la suma es {sumar(5,2)}')
+
+#Funcion lambda que sume dos numeros
+
+sumarLambda= lambda numero1,numero2:numero1+numero2
+
+print(f'la suma es {sumarLambda(5,2)}')
