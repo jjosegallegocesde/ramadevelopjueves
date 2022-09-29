544571860a18d3f486b9d3e2e6cf336c35048122
308 <308@academicos.cesde.edu.co>
2022-09-29 07:17:04 -0500

    NJ9 Se crea un funcion tradicional y lambda para calcular una suma

--- DagoMolina.py
+++ DagoMolina.py
@@ -1,1 +1,12 @@
 #("Funciones LambDa")
+
+def saludar(nombre):
+    return nombre
+    
+print(saludar("Dago Molina"))
+
+#TRASFORMANDO EN LAMBDA FUNCTION
+#Declarando funcion
+saludar2 = lambda nombre : nombre
+#Invocar funcion lambda
+print(saludar2("Julian"))
---
+++ funcion1.py
@@ -0,0 +1,10 @@
+#funcion que sume dos numero
+def calcular (numero1, numero2):
+    return numero1 + numero2
+#Invocar
+print(calcular(2, 2))
+
+#funcion lambda
+calcular2 = lambda numero1, numero2 : numero1 + numero2
+#invocar funcion lambda
+print(calcular2(4, 4))
