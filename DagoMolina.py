6b4fbebc180e2f2450a105654f6cf2e66b7da53f
308 <308@academicos.cesde.edu.co>
2022-09-29 07:35:58 -0500

    Creando funcion de multiplicacion * 100 asigando cualquier numero

--- DagoMolina.py
+++ DagoMolina.py
@@ -10,3 +10,15 @@ print(saludar("Dago Molina"))
 saludar2 = lambda nombre : nombre
 #Invocar funcion lambda
 print(saludar2("Julian"))
+
+#Retos
+#1) Función lambda que clasifique si un numero es par
+#Funcion Tradicional
+def calcularNumeroPar(numero):
+    if numero / 2:
+        return 'El numero es par', numero
+    else:
+        return 'el numero es impar'
+print(calcularNumeroPar(2))
+
+#Funcion lambda
---
+++ funcion2.py
@@ -0,0 +1,8 @@
+#Funcion tradicional
+def calcularMultiplicacion(numero):
+    return numero * 100
+print(calcularMultiplicacion(20))
+
+#Funcion lambda
+calcularMultiplicacion2 = lambda numero: numero * 100
+print(calcularMultiplicacion2(10))
