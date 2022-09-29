12409e7e0d0cb295acc417fd030c4e954e1f4e8b
edizonchavi <edizonchavi@gmail.com>
2022-09-29 07:22:28 -0500

    NTj09 se crean funciones lambda

--- EdisonChavarria.py
+++ EdisonChavarria.py
@@ -1,1 +1,11 @@
 #("funciones lamda")
+
+def saludar(nombre):
+    return nombre
+
+print(saludar("juan")) 
+
+#transformando en lamnda function
+
+saludar2=lambda nombre:nombre
+print(saludar("juan")) 
