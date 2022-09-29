6f8f46341867aabf18bf57bed90f31a670a942eb
Matiiu <mateotaveraramirez@gmail.com>
2022-09-29 08:07:25 -0500

    NTJ9 Se hace función multiplicar hasta 100

--- funcion2.py
+++ funcion2.py
@@ -5,9 +5,16 @@ def multiplicar(n):
     cont = 0
     while cont < 100:
         cont = cont + 1
-        print(f'{n} x {cont} = {n * cont} ')
+        print(f"{n} x {cont} = {n * cont} ")
     else:
         print('Adios :)')
 
+multiplicar(2)
 
-print(multiplicar(2))
+
+
+def multiplicarpor100(n):
+
+   print(f" El resultado es {n * 100} ") 
+
+multiplicarpor100(2)
