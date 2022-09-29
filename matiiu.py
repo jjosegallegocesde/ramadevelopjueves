4058602c1a21e11fbf46d1d0c78836a25cdbb970
Matiiu <mateotaveraramirez@gmail.com>
2022-09-29 07:30:57 -0500

    Se hace función suma

--- matiiu.py
+++ matiiu.py
@@ -11,3 +11,18 @@ def saludar2(nombre): return nombre
 
 
 print(saludar2('Matiiu'))
+
+#Función lambda que clasifique si un numero es par
+
+def numeroPar(n):
+
+    if n % 2 == 0:
+        print(f'El número {n} es par')
+    else:
+        print('El número no es par')    
+
+numeroPar()
+
+
+   
+
