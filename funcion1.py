4f2f41b3743124051842fb99dc75c4d7fe36f697
Manuela Taborda <manuelataborda48@gmail.com>
2022-09-29 08:14:10 -0500

    NTJ9 funciones

--- funcion1.py
+++ funcion1.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #Funcion suma 2 numeros
 def sumaNumeros (numero1, numero2):
     return numero1+numero2
@@ -12,7 +12,7 @@ sumarLambda= lambda numero1,numero2:numero1+numero2
 
 suma=sumarLambda(2,9)
 print('la suma es: ',suma)
-=======
+
 #funcion que sume dos numeros
 def sumarNumeros(numero1,numero2):
     return numero1+numero2
@@ -23,4 +23,4 @@ print('la sumas es: ',suma)
 sumarLambda=lambda numero1,numero2:numero1+numero2
 suma=sumarLambda(5,2)
 print('la sumas es: ',suma) 
->>>>>>> 2a981df292c8f6656adac92b42b3a55a8571f2d8
+
--- funcion2.py
+++ funcion2.py
@@ -1,4 +1,4 @@
 #Funcion de  todo los numero *100
 def numerom(numero):
     if(numero*100):
-        print("resultado es: ")
+        print("resultado es: ",numero)
