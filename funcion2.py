51175871da02f045144408c7282fa7cd227d9801
DANIELAESTRADA14 <daniestragomez@hotmail.com>
2022-09-29 07:41:49 -0500

    NTJ9 se crea funcion de multiplicar

---
+++ funcion2.py
@@ -0,0 +1,12 @@
+#Función que multiplique todo número por 100
+def multiplicar(numero):
+    return numero * 100
+
+
+resultado = multiplicar(5)
+print(resultado)
+
+#Lambda
+multiplicar2 = lambda numero: numero * 100
+total = multiplicar2(5)
+print(total)
