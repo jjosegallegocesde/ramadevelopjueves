af8b4f1d67291b47dfbd23f40ef1584744af5339
Manuela Taborda <manuelataborda48@gmail.com>
2022-09-29 07:36:50 -0500

    NTJ9 Se crea funcion par

--- manuelataborda.py
+++ manuelataborda.py
@@ -14,3 +14,14 @@ saludar2=lambda nombre:nombre
 
 #Invocar/llamar funcion   
 print (saludar2("Juan"))
+
+#Funcion que clasifique par
+def nmeropar(numero):
+    if (numero % 2 ==0):
+     print("El número es par")
+    else:
+        print("El número es impar")
+
+    npar=nmeropar(8)
+    print('el numero par es: ',npar)
+
