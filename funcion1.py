ac2d113d7403e0e051ea443b576a2aa6f4a2e753
Matiiu <mateotaveraramirez@gmail.com>
2022-09-29 08:12:22 -0500

    Se actualiza la rama

--- funcion1.py
+++ funcion1.py
@@ -1,18 +1,3 @@
-# Función que sume 2 numeros
-
-def suma(n1, n2):
-
-    return (f"{n1} + {n2} = {n1 + n2}")
-
-
-print(suma(5, 5))
-
-# Función lambda que suma dos números
-suma2 = lambda n1, n2: n1 + n2
-
-print(suma2(10, 10))
-
-
 #funcion que sume dos numeros
 def sumarNumeros(numero1,numero2):
     return numero1+numero2
