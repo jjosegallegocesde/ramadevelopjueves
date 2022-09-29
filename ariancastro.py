6662d96076d621b181d8277ef63fcfe6a62ede46
308 <308@academicos.cesde.edu.co>
2022-09-29 07:22:18 -0500

    se crean funciones lambda

--- ariancastro.py
+++ ariancastro.py
@@ -1,1 +1,15 @@
 #("funciones lamda")
+
+#declarar
+def saludar(nombre):
+    return nombre
+
+#invocar / llamar
+print(saludar("arian"))
+
+#tranformando en lambda function
+
+#declarando
+saludar2 = lambda nombre:nombre
+#invocar / llamar
+print(saludar2("arian"))
---
+++ funcion1.py
@@ -0,0 +1,8 @@
+#funcion que sume dos numeros
+def sumarNumeros(numero1, numero2):
+    return numero1+numero2
+print (sumarNumeros(5,2))
+
+#Funcion lambda que sume dos numeros
+sumarLambda=lambda numero1,numero2:numero1 + numero2
+print (sumarNumeros(5,2))
