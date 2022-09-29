b4e59f1db43319f34417cb08cbedd58118910341
308 <308@academicos.cesde.edu.co>
2022-09-29 07:18:27 -0500

    NTJ se crea funciones lambda

--- Nicolas.py
+++ Nicolas.py
@@ -1,1 +1,13 @@
 #print("funciones lambda")
+
+
+def saludar(nombre):
+    return f"hola {nombre}"
+
+print(saludar("Nicolas"))
+
+#transformado en lamda
+
+areaT = lambda nom: nom
+
+print(areaT("Nicolas"))
---
+++ funcion1.py
@@ -0,0 +1,6 @@
+#funcion que sume dos numeros
+sumar = lambda a,b : a+b
+print(sumar(5,1))
+
+
+
